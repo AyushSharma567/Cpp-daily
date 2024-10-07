cb5107380110386dcb36915041b31bec368d53a9
Ayush <ayush181101@gmail.com>
2024-10-07 09:20:02 +0530

    2696

---
+++ 2696_MinimumStringLengthAfterRemovingSubstrings.cpp
@@ -0,0 +1,16 @@
+//question https://leetcode.com/problems/minimum-string-length-after-removing-substrings/
+
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (!st.empty() && ((st.top() == 'A' && c == 'B') || (st.top() == 'C' && c == 'D'))) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.size();
+    }
+};
