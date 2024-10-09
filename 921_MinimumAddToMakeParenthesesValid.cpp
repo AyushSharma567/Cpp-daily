3bd3ee3470e61435cc77ce06a7733331febfa5af
Ayush <ayush181101@gmail.com>
2024-10-09 23:28:14 +0530

    921

---
+++ 921_MinimumAddToMakeParenthesesValid.cpp
@@ -0,0 +1,19 @@
+//question :- https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int left = 0, right = 0;
+        for (char c : s) {
+            if (c == '(') 
+                left++;
+            else {
+                if (left > 0)
+                    left--;
+                else 
+                    right++;
+            }
+        }
+        return left + right;
+    }
+};
