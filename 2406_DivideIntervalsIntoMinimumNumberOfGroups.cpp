f16da980dfe9dbfeafd55751ea4fb79d02f5c27a
Ayush <ayush181101@gmail.com>
2024-10-12 08:26:24 +0530

    2406

---
+++ 2406_DivideIntervalsIntoMinimumNumberOfGroups.cpp
@@ -0,0 +1,18 @@
+//question :- https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/
+
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        map<int, int> events;
+        for (const auto& interval : intervals) {
+            events[interval[0]]++;
+            events[interval[1] + 1]--;
+        }
+        int maxGroups = 0, currentGroups = 0;
+        for (const auto& event : events) {
+            currentGroups += event.second;
+            maxGroups = max(maxGroups, currentGroups);
+        }
+        return maxGroups;
+    }
+};
