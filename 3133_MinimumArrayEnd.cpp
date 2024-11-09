941947bcfff5a4cc222fb3630be7b5263b5dce1e
Ayush <ayush181101@gmail.com>
2024-11-09 19:43:02 +0530

    3133

---
+++ 3133_MinimumArrayEnd.cpp
@@ -0,0 +1,11 @@
+//question: https://leetcode.com/problems/minimum-array-end/
+
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long a = x;
+        while (--n)
+            a = (a + 1) | x;
+        return a;
+    }
+};
