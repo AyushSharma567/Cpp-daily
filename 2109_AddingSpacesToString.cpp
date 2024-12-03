684116c5a02fa8f11a86fbc39cc3a2f4b6ac4c67
Ayush <ayush181101@gmail.com>
2024-12-03 19:17:24 +0530

    2109

---
+++ 2109_AddingSpacesToString.cpp
@@ -0,0 +1,19 @@
+//question: https://leetcode.com/problems/adding-spaces-to-a-string/
+
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string result;
+        result.reserve(s.size() + spaces.size());
+        int spaceIndex = 0;
+        for (int stringIndex = 0; stringIndex < s.size(); ++stringIndex) {
+            if (spaceIndex < spaces.size() &&
+                stringIndex == spaces[spaceIndex]) {
+                result += ' ';
+                ++spaceIndex;
+            }
+            result += s[stringIndex];
+        }
+        return result;
+    }
+};
