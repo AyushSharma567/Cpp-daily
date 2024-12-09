72e2d62681acd75bf2b6d451abac394d22009404
Ayush <ayush181101@gmail.com>
2024-12-09 22:14:54 +0530

    3152

---
+++ 3152_SpecialArrayII.cpp
@@ -0,0 +1,24 @@
+//question: https://leetcode.com/problems/special-array-ii/
+
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> maxReach(n);
+        int end = 0;
+        for (int start = 0; start < n; start++) {
+            end = max(end, start);
+            while (end < n - 1 && nums[end] % 2 != nums[end + 1] % 2) 
+                ++end;
+            maxReach[start] = end;
+        }
+        vector<bool> ans(queries.size());
+        for (int i = 0; i < queries.size(); i++) {
+            vector<int> query = queries[i];
+            int start = query[0], end = query[1];
+            ans[i] = end <= maxReach[start];
+        }
+        return ans;
+    }
+};
+
