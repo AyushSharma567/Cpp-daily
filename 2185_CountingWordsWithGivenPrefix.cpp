5cf3b52d86d4962191e26f0723fcd409d281666b
Ayush <ayush181101@gmail.com>
2025-01-09 22:44:21 +0530

    2185

---
+++ 2185_CountingWordsWithGivenPrefix.cpp
@@ -0,0 +1,13 @@
+//question:https://leetcode.com/problems/counting-words-with-a-given-prefix/
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+        for (string& word : words) {
+            if (word.find(pref) == 0) 
+                count++;  
+        }
+        return count;
+    }
+};
